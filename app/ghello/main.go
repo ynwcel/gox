a362578851cf5c0829edf52c1f3a2ad7c778a7b3
黑枫叶 <454383818@qq.com>
2024-01-19 17:37:23 +0800

    test-ghello

--- app/ghello/main.go
+++ app/ghello/main.go
@@ -1,7 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hello")
+	fmt.Println(fmt.Sprintf("Hello -- %s", time.Now().Format("2006-01-02 15:04:05.000")))
 }
