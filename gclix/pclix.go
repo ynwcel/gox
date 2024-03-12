4fbd6d16fd66fc6b499e095540f439cd699e36e1
黑枫叶 <454383818@qq.com>
2024-03-12 16:11:18 +0800

    update

---
+++ gclix/pclix.go
@@ -0,0 +1,44 @@
+package pclix
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/fatih/color"
+	"github.com/urfave/cli/v2"
+)
+
+func NewApp(appName, appVersion string) *cli.App {
+	app := cli.NewApp()
+	app.Name = appName
+	app.Version = appVersion
+	app.Usage = fmt.Sprintf("V%s", appVersion)
+
+	app.ExitErrHandler = func(cCtx *cli.Context, err error) {
+		if err != nil {
+			var (
+				cmdName = cCtx.Command.Name
+			)
+			color.Red("Error:%s", err.Error())
+			if appName != cmdName {
+				color.Green("please use `%s %s --help` get more", appName, cmdName)
+			} else {
+				color.Green("please use `%s --help` get more", appName)
+			}
+			os.Exit(3)
+		}
+	}
+	app.OnUsageError = func(cCtx *cli.Context, err error, isSubcommand bool) error {
+		return err
+	}
+	return app
+}
+
+func NewCmd(cmdName string) *cli.Command {
+	return &cli.Command{
+		Name: cmdName,
+		OnUsageError: func(cCtx *cli.Context, err error, isSubcommand bool) error {
+			return err
+		},
+	}
+}
